182e906ecaf8f4312ae0de3501866bc6636a75ea
Arjen Adams <arjencpa15@gmail.com>
2024-11-15 14:03:03 +0100

    Poort veranderd naar 8000

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Haal de poort op uit omgevingsvariabelen, standaard naar 5000
-    port = int(os.environ.get("PORT", 5000))
+    # Haal de poort op uit omgevingsvariabelen, standaard naar 8000
+    port = int(os.environ.get("PORT", 8000))
 
     # Start de app op host 0.0.0.0 en de opgegeven port
     app.run(host='0.0.0.0', port=port)
