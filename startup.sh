71e231bfaa138bba35b2d8e18649bbefa7fb0888
Arjen Adams <arjencpa15@gmail.com>
2024-11-15 16:50:21 +0100

    Comments in startupscript

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
+echo "Installing requirements..." >> /home/site/wwwroot/startup.log
 pip install -r requirements.txt  # Installeer de dependencies
+echo "Starting run.py" >> /home/site/wwwroot/startup.log
 python run.py  # Start de Flask-app met de ontwikkelserver
